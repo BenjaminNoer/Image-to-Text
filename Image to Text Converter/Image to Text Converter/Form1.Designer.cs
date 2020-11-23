b27018dcf59baf893a4e1a9fd5a32b8bfc87a10c
Benjamin Noer <60319493+BenjoKazooie@users.noreply.github.com>
2020-11-23 09:58:04 +0000

    Add files via upload

---
+++ Image to Text Converter/Image to Text Converter/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Image_to_Text_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.NUDquality = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.browserMain = new System.Windows.Forms.WebBrowser();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDquality)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(591, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 339);
+            this.progressBar.Maximum = 1000;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(681, 15);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 15;
+            // 
+            // NUDquality
+            // 
+            this.NUDquality.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NUDquality.ForeColor = System.Drawing.Color.DarkRed;
+            this.NUDquality.Location = new System.Drawing.Point(447, 7);
+            this.NUDquality.Maximum = new decimal(new int[] {
+            1250,
+            0,
+            0,
+            0});
+            this.NUDquality.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.NUDquality.Name = "NUDquality";
+            this.NUDquality.Size = new System.Drawing.Size(52, 20);
+            this.NUDquality.TabIndex = 14;
+            this.NUDquality.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.NUDquality.ValueChanged += new System.EventHandler(this.NUDquality_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(372, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Image quality";
+            // 
+            // txtPath
+            // 
+            this.txtPath.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtPath.Location = new System.Drawing.Point(98, 7);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(268, 20);
+            this.txtPath.TabIndex = 12;
+            this.txtPath.Text = "Image path goes here";
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.browserMain);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(657, 291);
+            this.panel1.TabIndex = 11;
+            // 
+            // browserMain
+            // 
+            this.browserMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserMain.Location = new System.Drawing.Point(0, 0);
+            this.browserMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browserMain.Name = "browserMain";
+            this.browserMain.Size = new System.Drawing.Size(657, 291);
+            this.browserMain.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 5);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(80, 23);
+            this.btnBrowse.TabIndex = 10;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(505, 5);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(80, 23);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 354);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.NUDquality);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnConvert);
+            this.MinimumSize = new System.Drawing.Size(697, 392);
+            this.Name = "Form1";
+            this.Text = "Image to Text Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.NUDquality)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.NumericUpDown NUDquality;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser browserMain;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Image to Text Converter/Image to Text Converter/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace Image_to_Text_Converter
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", "&nbsp;" };
+        private string Content;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileBrowser = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.png, *.bmp) | *.jpg; *.jpeg; *.jpe; *.png; *.bmp",
+                RestoreDirectory = true
+            };
+            if (fileBrowser.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = fileBrowser.FileName;
+            }
+        }
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            btnConvert.Enabled = false;
+            try
+            {
+                progressBar.Value = 0;
+                Bitmap image = new Bitmap(txtPath.Text, true);
+                image = GetResizedImage(image, Convert.ToInt32(NUDquality.Value));
+                Content = ConvertToAscii(image);
+                browserMain.DocumentText = "<pre>" + Content + "</pre>";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + exception.Message);
+            }
+            btnConvert.Enabled = true;
+        }
+        private static Bitmap GetResizedImage(Bitmap inputBitmap, int asciiWidth)
+        {
+            int asciiHeight = (int)Math.Ceiling((double)inputBitmap.Height * asciiWidth / inputBitmap.Width);
+            Bitmap result = new Bitmap(asciiWidth, asciiHeight);
+            Graphics graphics = Graphics.FromImage(result);
+            graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            graphics.DrawImage(inputBitmap, 0, 0, asciiWidth, asciiHeight);
+            graphics.Dispose();
+            return result;
+        }
+        private string ConvertToAscii(Bitmap inputBitmap)
+        {
+            bool toggle = false;
+            progressBar.Maximum = inputBitmap.Width * inputBitmap.Height;
+            StringBuilder sb = new StringBuilder();
+            for (int h = 0; h < inputBitmap.Height; h++)
+            {
+                for (int w = 0; w < inputBitmap.Width; w++)
+                {
+                    Color pixelColor = inputBitmap.GetPixel(w, h);
+                    Color grayColor = Color.FromArgb((pixelColor.R + pixelColor.G + pixelColor.B) / 3, (pixelColor.R + pixelColor.G + pixelColor.B) / 3, (pixelColor.R + pixelColor.G + pixelColor.B) / 3);
+                    if (!toggle)
+                    {
+                        sb.Append(AsciiChars[(grayColor.R * 10) / 255]);
+                    }
+                    progressBar.PerformStep();
+                }
+                if (!toggle)
+                {
+                    sb.Append("<BR>");
+                    toggle = true;
+                }
+                else
+                {
+                    toggle = false;
+                }
+            }
+            return sb.ToString();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(browserMain.DocumentText))
+                {
+                    SaveFileDialog saveFile = new SaveFileDialog
+                    {
+                        FileName = "untitled",
+                        Filter = "Hyper Text Markup Language (.html)|*.html|Hyper Text Markup Language (.htm)|*.htm|Text File (.txt)|*.txt|Text Document (.docx)|*.docx|Text Document (.doc)|*.doc|Rich Text Format (.rtf)|*.rtf",
+                        FilterIndex = 1,
+                        RestoreDirectory = true
+                    };
+                    if (saveFile.ShowDialog() == DialogResult.OK)
+                    {
+                        StreamWriter writer = new StreamWriter(saveFile.OpenFile());
+                        if (saveFile.FilterIndex == 1 || saveFile.FilterIndex == 2)
+                        {
+                            writer.Write("<pre>" + Content + "</pre>");
+                        }
+                        else if (saveFile.FilterIndex == 3 || saveFile.FilterIndex == 4 || saveFile.FilterIndex == 5 || saveFile.FilterIndex == 6)
+                        {
+                            String[] splitter = new String[] { "<BR>" };
+                            string newContent = Content.Replace("&nbsp;", " ");
+                            String[] browserLines = newContent.Split(splitter, StringSplitOptions.None);
+                            writer.WriteLine("<pre>");
+                            for (int i = 0; i < browserLines.Length; i++)
+                            {
+                                writer.WriteLine(browserLines[i] + "<BR>");
+                            }
+                            writer.WriteLine("</pre>");
+                        }
+                        writer.Dispose();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There is no text to save." + Environment.NewLine + "Select a picture and convert it to text first.");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + exception.Message);
+            }
+        }
+        private void NUDquality_ValueChanged(object sender, EventArgs e)
+        {
+            if (NUDquality.Value <= 415)
+            {
+                NUDquality.ForeColor = Color.DarkRed;
+            }
+            else if (NUDquality.Value > 415 && NUDquality.Value <= 830)
+            {
+                NUDquality.ForeColor = Color.DarkOrange;
+            }
+            else
+            {
+                NUDquality.ForeColor = Color.DarkGreen;
+            }
+        }
+        private void txtPath_Enter(object sender, EventArgs e)
+        {
+            if (txtPath.Text == "Image path goes here")
+            {
+                txtPath.Clear();
+            }
+        }
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            txtPath.ForeColor = Color.Black;
+        }
+    }
+}
